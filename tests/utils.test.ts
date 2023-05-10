dfef574fe420a0738ebe90f27557fc856b8fdee3
Nicholas Humfrey <njh@aelius.com>
2023-05-10 22:36:25 +0100

    Switched to using jest expect, instead of assert for tests

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,41 +1,40 @@
 import { isDST } from '../lib/utils'
-import * as assert from 'assert'
 
 describe('Utility function Tests', function () {
   describe('isDST()', function () {
     it('2020-03-28 is not DST', function () {
       const date = new Date('2020-03-28 12:00:00')
-      assert.equal(isDST(date), false)
+      expect(isDST(date)).toBeFalsy()
     })
 
     it('2020-03-29 is DST', function () {
       const date = new Date('2020-03-29 12:00:00')
-      assert.equal(isDST(date), true)
+      expect(isDST(date)).toBeTruthy()
     })
 
     it('2020-08-01 is DST', function () {
       const date = new Date('2020-08-01 12:00:00')
-      assert.equal(isDST(date), true)
+      expect(isDST(date)).toBeTruthy()
     })
 
     it('2020-10-24 is DST', function () {
       const date = new Date('2020-10-24 12:00:00')
-      assert.equal(isDST(date), true)
+      expect(isDST(date)).toBeTruthy()
     })
 
     it('2020-10-25 is DST at 1am', function () {
       const date = new Date('2020-10-25 01:00:00')
-      assert.equal(isDST(date), true)
+      expect(isDST(date)).toBeTruthy()
     })
 
     it('2020-10-25 is not DST at 12pm', function () {
       const date = new Date('2020-10-25 12:00:00')
-      assert.equal(isDST(date), false)
+      expect(isDST(date)).toBeFalsy()
     })
 
     it('2021-01-01 is not DST', function () {
       const date = new Date('2021-01-01 12:00:00')
-      assert.equal(isDST(date), false)
+      expect(isDST(date)).toBeFalsy()
     })
   })
 })
