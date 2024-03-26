dc40e2d9763781621a954a3125098348d334f7ec
Nicholas Humfrey <njh@aelius.com>
2024-03-26 22:59:59 +0000

    Added comments to the configuration shell script example

--- examples/configure-theromostat.sh
+++ examples/configure-theromostat.sh
@@ -7,15 +7,15 @@
 export HMMB_DEVICE=/dev/ttyUSB0
 export HMMB_ID=1
 
-hmmb set-time
-hmmb set-auto-dst on
+hmmb set-time                        # Synchronise the system clock to the thermostat
+hmmb set-auto-dst on                 # Enable switching to/from DST automatically
 
-hmmb set-units 'C'
-hmmb set-switching-differential 1.0
-hmmb set-output-delay 0
-hmmb set-up-down-limit 0
-hmmb set-floor-limit 28         # Maximum allowed temperature for the floor sensor
-hmmb set-sensors 0              # 0 = Built in Sensor and optional Remote Air
-hmmb set-programme-mode none    # Disable schedule/programme mode
+hmmb set-units 'C'                   # Set the temperature units to Celsius
+hmmb set-switching-differential 1.0  # Set the switching to 1°C
+hmmb set-output-delay 0              # Disable the output delay
+hmmb set-up-down-limit 0             # Disable up-down temperature buttons limit
+hmmb set-floor-limit 28              # Maximum allowed temperature for the floor sensor
+hmmb set-sensors 0                   # 0 = Built in Sensor and optional Remote Air
+hmmb set-programme-mode none         # Disable schedule/programme mode
 
 hmmb get-status
